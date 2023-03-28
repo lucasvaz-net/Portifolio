be9cb9472ed908e531125bbbb46eab29abb0fa95
AVT <AVT@AVATIM>
2023-03-28 18:04:21 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProjetosController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Portifolio.Models;
+using static Portifolio.Models.ProjetosModel;
+
+namespace Portifolio.Controllers
+{
+    public class ProjetosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProjetosController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+        }
+
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            var apiUri = "https://api.github.com/repos/lucasvaz-net/PythonAPI/projects";
+
+            var response = _httpClient.GetAsync(apiUri).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var gitHubProjectApiModel = response.Content.ReadAsAsync<GitHubProjectApiModel>().Result;
+                var projetosModel = new ProjetosModel
+                {
+                    Nome = gitHubProjectApiModel.Name,
+                    Descricao = gitHubProjectApiModel.Description,
+                    Url = gitHubProjectApiModel.Url
+                };
+
+                var projects = new List<ProjetosModel> { projetosModel };
+                return Json(projects);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                return StatusCode((int)response.StatusCode, content);
+            }
+        }
+    }
+}
---
+++ Models/ProjetosModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Portifolio.Models
+{
+    public class ProjetosModel
+    {
+        public string? Nome { get; internal set; }
+        public string? Descricao { get; internal set; }
+        public string? Url { get; internal set; }
+
+        public class GitHubProjectApiModel
+        {
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+            public string? Url { get; set; }
+        }
+    }
+}
